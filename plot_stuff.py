29363f02a7b885bea2af227aa5bcbfb339454522
Nuria Nav <nuria.navarronav@mail.huji.ac.il>
2023-06-21 12:27:40 +0300

    Added input for the folder to plot

--- plot_stuff.py
+++ plot_stuff.py
@@ -3,69 +3,8 @@ import merge_history as mh
 import generate_file_models as gfm
 
 def main():
-    folder = '2M_tests'
+    folder = input("Folder name: ")
     sim_folder = '/Users/nunina/MESA/Simulations/'
-
-    m3_tests = {
-        'to ZAMS' : [
-            {
-                'folder_path' : sim_folder + '3M_tests/LOGS_1',
-                'name' : 'to ZAMS',
-                'models' : [1000,10476],
-            },
-        ],
-        'to TAMS' : [
-            {
-                'folder_path' : sim_folder + '3M_tests/LOGS_2',
-                'name' : 'to TAMS',
-                'models' : [1000,10476],
-            },
-        ],
-        'to TACHeB' : [
-            {
-                'folder_path' : sim_folder + '3M_tests/LOGS_3',
-                'name' : 'to TACHeB',
-                'models' : [1000,10476],
-            },
-        ],
-    }
-    m3_overshoot_res = {
-        #'to ZAMS' : [
-        #    {
-        #        'folder_path' : sim_folder + '3M_overshoot_res/LOGS_1',
-        #        'name' : 'to ZAMS',
-        #        'models' : [1000,10476],
-        #    },
-        #],
-        #'to TAMS' : [
-        #    {
-        #        'folder_path' : sim_folder + '3M_overshoot_res/LOGS_2',
-        #        'name' : 'to TAMS',
-        #        'models' : [1000,10476],
-        #    },
-        #],
-        #'to TACHeB' : [
-        #    {
-        #        'folder_path' : sim_folder + '3M_overshoot_res/LOGS_3',
-        #        'name' : 'to TACHeB',
-        #        'models' : [1000,10476],
-        #    },
-        #],
-        'to AGB_ov_res' : [
-            {
-                'folder_path' : sim_folder + '3M_overshoot_res/LOGS_4',
-                'name' : 'to AGB',
-                'models' : [1000,10476],
-            },
-        ],
-        'to AGB_ov' : [
-            {
-                'folder_path' : sim_folder + '3M_overshoot_res/',
-                'name' : 'to AGB',
-                'models' : [1000,10476],
-            },
-        ],
-    }
 
     to_plot = gfm.generate_file_models(sim_folder + folder)
     mh.all_time(to_plot)
